0fd57738fa0a314ccefa7696d46119df9d7b81ae
Pankaj Vishwakarma <vishwapa@outlook.com>
2026-03-05 07:52:23 +0530

    feat(cloudinary): support dynamic folder base separation for clients

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-        folder: 'ignisguard_companies',
+        folder: process.env.CLOUDINARY_FOLDER || 'ignisguard_companies',
         allowed_formats: ['jpg', 'png', 'jpeg'],
     },
 });
